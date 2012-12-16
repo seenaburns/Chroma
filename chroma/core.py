69614e94ae4481ced88941d0a55fb4843d280b2b
Seena Burns <seenaburns@github>
2012-12-16 11:36:37 -0800

    Add HLS, HSV support. Add setter methods (rgb, hls, hsv)

--- chroma/core.py
+++ chroma/core.py
@@ -8,11 +8,7 @@ Provides Color object
 
 """
 
-# On tuples and values:
-# Whenever possible try to accpet both tuples and values as arguments for
-# ease of use (FOR API), but try to impose tuples as arguments / returns
-#
-# Internally, tuples are assumed to be passed and returned
+# Tuples are assumed to be passed and returned
 
 import colorsys
 
@@ -21,11 +17,11 @@ class Color(object):
     Chroma Color object stores 'color value' to be given in any format
     by one of the properties
     """
-    def __init__(self, color_value, format = 'HEX'):
+    def __init__(self, color_value = '#FFFFFF', format = 'HEX'):
         # self.color is the main storage for the color in the format of a
         # tuple of RGB float
         if format == 'HEX':
-            self.color = self._rgb_from_hex(color_value)
+            self.rgb = self._rgb_from_hex(color_value)
 
     #
     # RGB
@@ -37,6 +33,16 @@ class Color(object):
     @property
     def rgb256(self):
         return map(lambda x: int(x*255), self.color)
+
+    @rgb.setter
+    def rgb(self, color_tuple):
+        """Used as main setter (rgb256, hls, hls256, hsv, hsv256)"""
+        # Check bounds
+        self.color = map(self._apply_float_bounds, color_tuple)
+
+    @rgb256.setter
+    def rgb256(self, color_tuple):
+        self.rgb = map(lambda x: x/255.0, color_tuple)
 
     #
     # HLS
@@ -51,6 +57,15 @@ class Color(object):
         r, g, b = self.rgb256
         return colorsys.rgb_to_hls(r, g, b)
 
+    @hls.setter
+    def hls(self, color_tuple):
+        h, l, s = color_tuple
+        self.rgb = colorsys.hls_to_rgb(h, l, s)
+
+    @hls256.setter
+    def hls256(self, color_tuple):
+        self.hls = map(lambda x: x/255.0, color_tuple)
+
     #
     # HSV
     #
@@ -63,6 +78,15 @@ class Color(object):
     def hsv256(self):
         r, g, b = self.rgb256
         return colorsys.rgb_to_hsv(r, g, b)
+
+    @hsv.setter
+    def hsv(self, color_tuple):
+        h, s, v = color_tuple
+        self.rgb = colorsys.hsv_to_rgb(color_tuple)
+
+    @hsv256.setter
+    def hsv256(self, color_tuple):
+        self.hsv = map(lambda x: x/255.0, color_tuple)
 
     #
     # HEX
@@ -86,7 +110,7 @@ class Color(object):
             hex_value = hex_value[1:]
 
         # Check length
-        if len(hex_value) > 6:
+        if len(hex_value) != 6:
             raise Exception('Invalid Hex Input: %s' % (color_value))
 
         # Return rgb from hex
@@ -98,7 +122,7 @@ class Color(object):
             raise Exception('Invalid Hex Input: %s' % (color_value))
 
     def _float_to_hex(self, float_value):
-        # Convert from float to in to hex, remove '0x'
+        # Convert from float to in to hex number, remove '0x'
         hex_value = hex(int(float_value*255))[2:]
 
         # If hex is only one digit, pad with 0
@@ -107,4 +131,11 @@ class Color(object):
 
         return hex_value
 
+    def _apply_float_bounds(self, coordinate):
+        """Assure coordinate is a float between 0 to 1"""
+        if coordinate < 0.0:
+            return 0.0
+        elif coordinate > 1.0:
+            return 1.0
 
+        return float(coordinate)
