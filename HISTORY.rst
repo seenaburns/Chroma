527975b1379fadd93e51fa60b5f4c23803cc243e
Seena Burns <seenaburns@github>
2012-12-15 20:50:29 -0800

    Add setup.py, HISTORY.rst

---
+++ HISTORY.rst
@@ -0,0 +1,7 @@
+History
+-------
+
+0.1.0 (2012-12-15)
+------------------
+- Initial Release
+- RGB and Hex support
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='Chroma',
+    version='0.1.0',
+    author='Seena Burns',
+    author_email='hello@seenaburns.com',
+    url='https://github.com/seenaburns/Chroma',
+    license=open('LICENSE.txt').read(),
+    description='Color handling made simple.',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    packages=['chroma'],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+)
